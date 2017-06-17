c078c8f0bea496f6a10f2c99ed43c1153fbca37b
Dinicley Alves <dinicleyalvess@gmail.com>
2017-06-17 11:10:01 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <title>Bem Vindos | @nomedosite</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="description" content="Dinicley Alves | Web Developer">
+    <link href="favicon.ico" rel="icon" type="image/x-icon" />    
+
+</head>
+
+<body>
+    
+    <div class="anuncio">
+        <h2>Seja Bem Vindo!</h2>
+        <h1>Em Breve aqui: @nomedosite</h1>
+    </div>
+    
+    <div class="mensagem">
+    
+                <h2>Em Construção!</h2>
+                <img src="img/tool.png" alt="tool" title="Em breve @nomedosite">
+                <h3><a href="#" target="_blank" title="Conheça a Agência/Desenvolvedor:   @NomeDaAgência"> @NomeDaAgencia Web - @Ano </a></h3>
+    </div>
+    
+    <footer class="copyright">
+
+        <p> &copy; @ano - Todos os Direitos Reservados | e-mail: contato@empresa.com | (55) 5555-5555</p>
+    
+    </footer>
+
+</body>
+</html>
